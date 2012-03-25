4252013ebc2c4dcbdaba3d662c9ad4ce8a2e39f8
Jorge Gonzalez <jag2kn@gmail.com>
2012-03-25 15:57:42 -0500

    * Se agrega primera versión del modelo  * Se agrega estructura de django

---
+++ leyes/seguimiento/admin.py
@@ -0,0 +1,10 @@
+from seguimiento.models import *
+from django.contrib import admin
+
+
+admin.site.register(Partido)
+admin.site.register(Senador)
+admin.site.register(Estado)
+admin.site.register(ProyectoLey)
+admin.site.register(Articulo)
+admin.site.register(Observaciones)
---
+++ leyes/seguimiento/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your Models here.
+class Partido(models.Model):
+	nombre = models.CharField(max_length=120)
+	def __unicode__(self):
+                return self.nombre
+
+class Senador(models.Model):
+	nombre = models.CharField(max_length=120)
+	partido = models.ForeignKey(Partido)
+	def __unicode__(self):
+                return self.nombre
+
+class Estado(models.Model):
+        nombre = models.CharField(max_length=120)
+        def __unicode__(self):
+                return self.nombre
+
+class ProyectoLey(models.Model):
+	nombre = models.CharField(max_length=120)
+	fechaRadicacion = models.DateField()
+	descripcion = models.CharField(max_length=120)
+	estado = models.ForeignKey(Estado)
+	ponentes = models.ManyToManyField(Senador)
+	def __unicode__(self):
+		return self.nombre
+
+class Articulo(models.Model):
+	nombre = models.CharField(max_length=200)
+	texto = models.TextField()
+	proyecto = models.ForeignKey(ProyectoLey)
+	def __unicode__(self):
+                return self.nombre
+
+	
+class Observaciones(models.Model):
+	autor = models.ForeignKey(User)
+	texto = models.TextField()
+	fecha = models.DateField()
+	def __unicode__(self):
+                return self.autor+" - "+self.texto[0:20]
+
+
